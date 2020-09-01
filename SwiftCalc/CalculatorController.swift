12c87dfd8fe7adaa656dabe82fdda84ce8f42da9
Eyal Avisar <enfer0666@yahoo.com>
2020-09-01 14:55:09 +0300

    calculation simplified adding firebase auth

--- SwiftCalc/CalculatorController.swift
+++ SwiftCalc/CalculatorController.swift
@@ -83,8 +83,9 @@ class CalculatorController: UIViewController {
     }
     
     func calculate() {
-        //i think it is better to work on operations and write it as
-        // X, /, +, -, +=, -=, X=... etc
+        print("numbers before: \(exercise.numbers)")
+        print("operations before: \(exercise.operations)")
+
         if exercise.numbers.count >= 2 {
             divideOrMultiply()
         }
@@ -93,78 +94,24 @@ class CalculatorController: UIViewController {
         }
         //TODO: write to firebase
         if user != nil {
-//            self.ref.child("users").child(user.uid).setValue(["username": username])
             ref?.child("users").child(user!.userName).child("exerciseIsDot").setValue(exercise.isDot)
             ref?.child("users").child(user!.userName).child("exerciseIsOperation").setValue(exercise.isOperation)
             ref?.child("users").child(user!.userName).child("exerciseNumbers").setValue(exercise.numbers)
             ref?.child("users").child(user!.userName).child("exerciseOperations").setValue(exercise.operations)
-            
-            ref?.child("users").child("exerciseIsOperation").setValue(exercise.isOperation)
-            ref?.child("users").child("exerciseNumbers").setValue(exercise.numbers.description)
-            ref?.child("users").child("exerciseOperations").setValue(exercise.operations.description)
         }
         
-
+        print("numbers: \(exercise.numbers)")
+        print("operations: \(exercise.operations)")
         resultLabel.text = exercise.numbers.last
         
-//        var newExercise = Exercise()
-//        var component = ""
-//
-//        for key in newExercise.variables {
-//            databaseHandle = ref?.child(key).observe(.value, with: { (snapshot) in
-//                switch key {
-//                case "exerciseNumbers":
-//                    component = snapshot.value as! String
-//                    let components = component.components(separatedBy: [",", "\""])
-//                    for item in components {
-//                        newExercise.numbers += [item]
-//                    }
-//                    var index = 0
-//                    while index < newExercise.numbers.count {
-//                        if Double(newExercise.numbers[index]) == nil {
-//                            newExercise.numbers.remove(at: index)
-//                            index -= 1
-//                        }
-//                        index += 1
-//                    }
-//
-//                case "exerciseIsOperations":
-//                    newExercise.isOperation = snapshot.value as? Bool ?? false
-//                case "exerciseIsDot":
-//                    newExercise.isDot = snapshot.value as? Bool ?? false
-//                case "exerciseOperations":
-//                    component = snapshot.value as! String
-//                    let components = component.components(separatedBy: [",", "\""])
-//                    for item in components {
-//                        if ["+", "-", "X", "/", "X=", "/=", "+=", "-="].contains(item) {
-//                            newExercise.operations += [item]
-//                        }
-//                    }
-//                default:
-//                    print("retrieval failed")
-//                }
-//
-//                print("component: \(component)")
-//                print("numbers: \(newExercise.numbers) \(newExercise.operations) dot \(newExercise.isDot) isop \(newExercise.isOperation)")
-//            })
-//        }
-
     }
     
     func addOrSubtract() {
         var result:Double?
-
-        let lastIndex = exercise.numbers.count
-        
-        for operation in 0..<lastIndex {
-            if exercise.operations[operation] == "X" || exercise.operations[operation] == "/" {
-                if exercise.numbers.count > operation {
-                    divideOrMultiply(index: operation)
-                }
-                return
-            }
+        if exercise.operations[1] == "X" || exercise.operations[1] == "/" {
+            divideOrMultiply(index: 1)
+            return
         }
-        
         if exercise.operations[0] == "+" {
             result = Double(exercise.numbers[0])! + Double(exercise.numbers[1])!
         }
@@ -203,47 +150,4 @@ class CalculatorController: UIViewController {
         exercise.operations.remove(at: index)
     }
     
-//    func setExerciseFromDatabase() {
-//        var component = ""
-//        
-//        for key in exercise.variables {
-//            databaseHandle = ref?.child(key).observe(.value, with: { (snapshot) in
-//                switch key {
-//                case "exerciseNumbers":
-//                    component = snapshot.value as! String
-//                    let components = component.components(separatedBy: [",", "\""])
-//                    for item in components {
-//                        self.exercise.numbers += [item]
-//                    }
-//                    var index = 0
-//                    while index < self.exercise.numbers.count {
-//                        if Double(self.exercise.numbers[index]) == nil {
-//                            self.exercise.numbers.remove(at: index)
-//                            index -= 1
-//                        }
-//                        index += 1
-//                    }
-//
-//                case "exerciseIsOperations":
-//                    self.exercise.isOperation = snapshot.value as? Bool ?? false
-//                case "exerciseIsDot":
-//                    self.exercise.isDot = snapshot.value as? Bool ?? false
-//                case "exerciseOperations":
-//                    component = snapshot.value as! String
-//                    let components = component.components(separatedBy: [",", "\""])
-//                    for item in components {
-//                        if ["+", "-", "X", "/", "X=", "/=", "+=", "-="].contains(item) {
-//                            self.exercise.operations += [item]
-//                        }
-//                    }
-//                default:
-//                    print("retrieval failed")
-//                }
-//                
-//                print("component: \(component)")
-//                print("numbers: \(self.exercise.numbers) \(self.exercise.operations) dot \(self.exercise.isDot) isop \(self.exercise.isOperation)")
-//            })
-//
-//        }
-//    }
 }
